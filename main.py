d40605f458fc9461c9bd13cde4765569d24a1f11
Charlie Lee <clee@tuta.io>
2022-10-07 07:52:45 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from purpleAirData import purpleAirData
+import time
+import os
+beep = lambda x: os.system("echo -n '\a';sleep 0.2;" * x)
+
+def main():
+    conditions = []
+    for condition in purpleAirData.conditions:
+        conditions.append(condition)
+    pad = purpleAirData("http://192.168.1.129/json", 30, conditions)
+
+    while (1==1):
+        print(pad.readings["pm2.5_aqi"])
+        if pad.readings["pm2.5_aqi"] > 10:
+            beep(3)
+            beep(3)
+            beep(3)
+        print(pad.readings)
+        time.sleep(5)
+    
+if __name__ == "__main__":
+    main()
